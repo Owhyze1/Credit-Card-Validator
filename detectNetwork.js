5aaa19489a33263882bc43c1439b52deaec71afe
Tiffany <Owhyze1@gmail.com>
2021-05-04 14:24:42 -0400

    Determines if network is Diner's Club, AMEX, Visa, or Mastercard

--- detectNetwork.js
+++ detectNetwork.js
@@ -15,6 +15,44 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+  if (cardNumber === undefined || cardNumber === '' || cardNumber.length < 2){
+    return '';
+  }
+
+  var prefix = Number(cardNumber.slice(0,2));
+  var length = cardNumber.length;
+  var networkName = '';
+  // console.log(`testing ${cardNumber}`);
+  // console.log(length);
+  // console.log( cardNumber[0]);
+
+  if (length === 14 && (prefix === 38 || prefix === 39)){
+      networkName = 'Diner\'s Club';
+  } else if (length === 15 && (prefix === 34 || prefix === 37)) {
+    networkName = 'American Express';
+  } else if ((length === 13 || length === 16 || length === 19) && cardNumber[0] === '4'){
+    networkName = 'Visa';
+  } else if (length === 16 && (prefix >= 51 && prefix <= 55)){
+    networkName = 'MasterCard';
+  }
+  // console.log(`network name: ${networkName}`);
+  return networkName;
 };
+
+// test functions
+
+// detectNetwork('38345678901234'); // Diner's Club
+// detectNetwork('39345678901234'); // Diner's Club
+// detectNetwork('343456789012345'); // American Express
+// detectNetwork('373456789012345'); // American Express
+// detectNetwork('4123456789012'); // Visa
+// detectNetwork('4123456789012345'); // Visa
+// detectNetwork('4123456789012345678'); // Visa
+// detectNetwork('5112345678901234') // MasterCard
+// detectNetwork('5212345678901234') // MasterCard
+// detectNetwork('5312345678901234') // MasterCard
+// detectNetwork('5412345678901234') // MasterCard
+// detectNetwork('5512345678901234') // MasterCard
+
 
 
